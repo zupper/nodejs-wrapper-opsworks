f6260a13990d1b26793ff286878824253c91436d
Dimitar Pavlov <dimitar@shadez.info>
2015-03-13 18:04:01 +0200

    Created initial version of the cookbook

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'nodejs-0.12.0-wrapper'
+maintainer       'Dimitar PAvlov'
+maintainer_email 'dimitar@shadez.info'
+license          'All rights reserved'
+description      'Installs/Configures nodejs-0.12.0'
+long_description 'Installs/Configures nodejs-0.12.0'
+version          '0.1.0'
+
+depends 'nodejs'
